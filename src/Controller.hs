9de41ba87b09ec7f04d9a5b1ac3b66b71250fc75
MWRuszczycky <mwruszczycky@gmail.com>
2020-01-27 11:14:28 -0600

    Removes unnecessary comments in Controller

--- src/Controller.hs
+++ src/Controller.hs
@@ -13,8 +13,4 @@ controller :: IO ()
 controller = do
     putStrLn "\nWeekly Sets"
     let jsets = J.journalSetsByYear 2019 R.issueRefs
-    --let (ws,ms) = J.splitByFreq R.issueRefs
-    --    wss     = map (J.issuesInYear 2019) ws
-    --    mss     = map (J.issuesInYear 2019) ms
-    --    jsets   = J.journalSets wss mss
     mapM_ ( \ j -> Tx.putStrLn (V.viewJournalSet j) >> putStrLn "" ) jsets
