8492f6a3293f6ab0a5b47005a4c90ebd9fd4eb28
MWRuszczycky <mwruszczycky@gmail.com>
2020-02-16 21:28:54 -0600

    Adds encoding specification to Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,17 @@
 
 module Main where
 
-import Control.Monad.Except ( runExceptT )
-import Control.Monad.Reader ( runReaderT )
-import System.Environment   ( getArgs    )
-import Controller           ( controller
-                            , configure
-                            , finish     )
+import qualified GHC.IO.Encoding                     as GHC
+import           Control.Monad.Except ( runExceptT )
+import           Control.Monad.Reader ( runReaderT )
+import           System.Environment   ( getArgs    )
+import           Controller           ( controller
+                                      , configure
+                                      , finish     )
 
 main :: IO ()
 main = do
+    GHC.setLocaleEncoding GHC.utf8
     etConfig <- configure <$> getArgs
     case etConfig of
          Left err -> putStrLn $ err <> "Try option '-h' or '--help' for usage."
