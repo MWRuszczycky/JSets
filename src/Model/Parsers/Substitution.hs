1b4ad02d8fd943dfdf5fab28137715d6fa348948
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-03 21:10:39 -0500

    Removes Either from the substition parser

--- src/Model/Parsers/Substitution.hs
+++ src/Model/Parsers/Substitution.hs
@@ -9,7 +9,6 @@ import qualified Data.Text            as Tx
 import qualified Data.Attoparsec.Text as At
 import qualified Data.Map.Strict      as Map
 import           Data.Text                   ( Text        )
-import           Data.Bifunctor              ( bimap       )
 import           Control.Applicative         ( (<|>), many )
 
 data AtBraced =
@@ -17,17 +16,16 @@ data AtBraced =
     | Braced Text
     | Unbraced Text deriving ( Eq, Show )
 
--- The parser does not fail, so we don't need to return an either.
-
-substitute :: Text -> Map.Map Text Text -> Text -> Either String Text
-substitute d xys = fmap (Tx.concat . map go) . parseAtBraced
+substitute :: Text -> Map.Map Text Text -> Text -> Text
+substitute d xys = Tx.concat . map go . parseAtBraced
     where go FreeAt       = "@"
           go (Unbraced y) = y
           go (Braced   x) = Map.findWithDefault d x xys
 
-parseAtBraced :: Text -> Either String [AtBraced]
-parseAtBraced = bimap err id . At.parseOnly ( many atBraced )
-    where err = (++) "Cannot parse at-braced text: "
+parseAtBraced :: Text -> [AtBraced]
+-- ^This parser never fails, so we have a dummy value in the either
+-- expression that just returns and empty list.
+parseAtBraced = either (const []) id . At.parseOnly ( many atBraced )
 
 atBraced :: At.Parser AtBraced
 atBraced = At.peekChar' >>= go
