22f13bf6bf17d898db19b44c810b40a80322d80b
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-19 20:42:35 -0500

    Adds foundation for JSON parsing

--- src/Model/Parsers/CSV.hs
+++ src/Model/Parsers/CSV.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Model.Parsers.CSV
-    ( parseCSV
+    ( JsonVal   (..)
+    , CSV
+    , parseCSV
+    , parseJSON
     ) where
 
 import qualified Data.Text            as Tx
@@ -10,11 +13,41 @@ import           Data.Text                   ( Text  )
 import           Data.Bifunctor              ( bimap )
 import           Control.Applicative         ( many  )
 
-parseCSV :: Text -> Either String [[Text]]
+-- =============================================================== --
+-- Types
+
+data JsonVal =
+      JStr   Text
+    | JNum   Text
+    | JBool  Bool
+    | JObj   [ (Text, JsonVal) ]
+    | JArray [ JsonVal ]
+      deriving ( Show )
+
+type CSV = [[Text]]
+
+-- =============================================================== --
+-- Exported parsers
+
+parseCSV :: Text -> Either String CSV
 parseCSV = bimap err id . At.parseOnly csv
     where err = (++) "Cannot parse CSV: "
 
-csv :: At.Parser [[Text]]
+-- =============================================================== --
+-- General parsers
+
+quoted :: At.Parser Text
+quoted = At.char '\"' *> go
+    where go = do x <- At.anyChar
+                  case x of
+                       '\\' -> Tx.cons <$> At.anyChar <*> go
+                       '\"' -> pure Tx.empty
+                       _    -> Tx.cons x <$> go
+
+-- =============================================================== --
+-- CSV Parser components
+
+csv :: At.Parser CSV
 csv = many csvLine <* At.endOfInput
 
 csvLine :: At.Parser [Text]
@@ -27,10 +60,12 @@ csvCell = do
          '\"' -> quoted
          _    -> At.takeTill $ flip elem [',', '\n', '\r']
 
-quoted :: At.Parser Text
-quoted = At.char '\"' *> go
-    where go = do x <- At.anyChar
-                  case x of
-                       '\\' -> Tx.cons <$> At.anyChar <*> go
-                       '\"' -> pure Tx.empty
-                       _    -> Tx.cons x <$> go
+-- =============================================================== --
+-- JSON parser components
+
+parseJSON :: Text -> Either String JsonVal
+parseJSON = bimap err id . At.parseOnly json
+    where err = (++) "Cannot parse JSON: "
+
+json :: At.Parser JsonVal
+json = undefined
