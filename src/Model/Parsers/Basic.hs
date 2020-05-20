34a43d041e6cb95112008fa6fbc8ca7b5dd77954
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-20 07:41:22 -0500

    Adds a JSON parser and fixes quoted string parsing
    1. Adds a JSON parser. This parser is more premissive than the
       specification at https://www.json.org/json-en.html with respect
       to number parsing. For example, 09 and +9 will parse as 9.0, when
       they should both fail to parse according to the spec.
    
    2. More correctly handles escape sequences in quoted strings. This
       affects both the JSON parser and the CSV parser.

--- src/Model/Parsers/Basic.hs
+++ src/Model/Parsers/Basic.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Model.Parsers.Basic
-    ( JsonVal   (..)
+    ( JSON      (..)
     , CSV
     , parseCSV
     , parseJSON
@@ -9,19 +9,22 @@ module Model.Parsers.Basic
 
 import qualified Data.Text            as Tx
 import qualified Data.Attoparsec.Text as At
-import           Data.Text                   ( Text  )
-import           Data.Bifunctor              ( bimap )
-import           Control.Applicative         ( many  )
+import           Data.Text                   ( Text              )
+import           Data.Bifunctor              ( bimap             )
+import           Control.Applicative         ( (<|>), many, some )
 
 -- =============================================================== --
 -- Types
 
-data JsonVal =
+data JSON =
       JStr   Text
     | JNum   Text
+    | JInt   Integer
+    | JDbl   Double
     | JBool  Bool
-    | JObj   [ (Text, JsonVal) ]
-    | JArray [ JsonVal ]
+    | JNull
+    | JObj   [ (Text, JSON) ]
+    | JArray [ JSON ]
       deriving ( Show )
 
 type CSV = [[Text]]
@@ -33,16 +36,49 @@ parseCSV :: Text -> Either String CSV
 parseCSV = bimap err id . At.parseOnly csv
     where err = (++) "Cannot parse CSV: "
 
+parseJSON :: Text -> Either String JSON
+parseJSON = bimap err id . At.parseOnly json
+    where err = (++) "Cannot parse JSON: "
+
 -- =============================================================== --
 -- General parsers
 
 quoted :: At.Parser Text
-quoted = At.char '\"' *> go
-    where go = do x <- At.anyChar
-                  case x of
-                       '\\' -> Tx.cons <$> At.anyChar <*> go
-                       '\"' -> pure Tx.empty
-                       _    -> Tx.cons x <$> go
+quoted = do
+    At.char '\"'
+    s <- many $ escaped <|> At.satisfy ( \ x -> x /= '\\' && x /= '\"' )
+    At.char '\"'
+    pure . Tx.pack $ s
+
+escaped :: At.Parser Char
+escaped = At.char '\\' *> At.choice [ At.char '\"' *> pure '\"'
+                                    , At.char '/'  *> pure '/'
+                                    , At.char 'b'  *> pure '\b'
+                                    , At.char 'f'  *> pure '\f'
+                                    , At.char 'n'  *> pure '\n'
+                                    , At.char 'r'  *> pure '\r'
+                                    , At.char 't'  *> pure '\t'
+                                    , At.char 'u'  *> pure 'u'
+                                    , At.char '\\' *> pure '\\'
+                                    ]
+
+comma :: At.Parser ()
+comma = At.skipSpace *> At.char ',' *> At.skipSpace
+
+leftBrace :: At.Parser ()
+leftBrace = At.skipSpace *> At.char '{' *> At.skipSpace
+
+rightBrace :: At.Parser ()
+rightBrace = At.skipSpace *> At.char '}' *> At.skipSpace
+
+leftBracket :: At.Parser ()
+leftBracket = At.skipSpace *> At.char '[' *> At.skipSpace
+
+rightBracket :: At.Parser ()
+rightBracket = At.skipSpace *> At.char ']' *> At.skipSpace
+
+colon :: At.Parser ()
+colon = At.skipSpace *> At.char ':' *> At.skipSpace
 
 -- =============================================================== --
 -- CSV Parser components
@@ -63,9 +99,59 @@ csvCell = do
 -- =============================================================== --
 -- JSON parser components
 
-parseJSON :: Text -> Either String JsonVal
-parseJSON = bimap err id . At.parseOnly json
-    where err = (++) "Cannot parse JSON: "
+json :: At.Parser JSON
+json = jsonObject <* At.endOfInput
 
-json :: At.Parser JsonVal
-json = undefined
+jsonObject :: At.Parser JSON
+jsonObject = do
+    leftBrace
+    kvps <- At.sepBy jsonKeyValuePair comma
+    rightBrace
+    pure . JObj $ kvps
+
+jsonKeyValuePair :: At.Parser (Text, JSON)
+jsonKeyValuePair = do
+    key <- quoted
+    colon
+    val <- jsonValue
+    pure (key, val)
+
+jsonValue :: At.Parser JSON
+jsonValue = jsonString
+            <|> jsonObject
+            <|> jsonArray
+            <|> jsonInteger
+            <|> jsonDouble
+            <|> jsonBool
+            <|> jsonNull
+
+jsonString :: At.Parser JSON
+jsonString = quoted >>= pure . JStr
+
+jsonArray :: At.Parser JSON
+jsonArray = do
+    leftBracket
+    vals <- At.sepBy jsonValue comma
+    rightBracket
+    pure . JArray $ vals
+
+jsonDouble :: At.Parser JSON
+-- ^Does not exactly follow the json specification, because it is
+-- more permissive. For example, 09 will and +9 will both parse as
+-- 9.0 when the parser should fail. See
+-- https://www.json.org/json-en.html
+jsonDouble = JDbl <$> At.double
+
+jsonInteger :: At.Parser JSON
+jsonInteger = do
+    x  <- At.char '-' <|> At.satisfy (At.inClass "123456789")
+    xs <- if x == '-' then some At.digit else many At.digit
+    pure . JInt . read $ x : xs
+
+jsonBool :: At.Parser JSON
+jsonBool = trueBool <|> falseBool
+    where trueBool  = At.string "true"  *> pure (JBool True)
+          falseBool = At.string "false" *> pure (JBool False)
+
+jsonNull :: At.Parser JSON
+jsonNull = At.string "null" *> pure JNull
