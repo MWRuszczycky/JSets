7d33ab10ad7e6ac2cd6c425ed6d5438fbc19cd66
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-22 19:48:10 -0500

    Fixes tests after the View/Formatting refactor

--- tests/ParserTests.hs
+++ tests/ParserTests.hs
@@ -7,7 +7,7 @@ import qualified Data.Text.IO              as Tx
 import qualified Model.Core.Types          as T
 import qualified Model.Journals            as J
 import qualified Model.Parsers.JournalSets as P
-import qualified Model.Text.Formatting     as F
+import qualified View.View                 as V
 import qualified TestReferences            as TR
 import           Test.Hspec                       ( Spec (..)
                                                   , hspec
@@ -24,8 +24,8 @@ testGenParse2019 :: Spec
 testGenParse2019 = it "Parses JSet2019 to yearly26Sets 2019" $ do
     let refs     = TR.issueRefs
         journals = map (T.abbr . T.journal) refs
-        expected = F.jsetsToCsv journals . J.yearly26Sets 2019 $ refs
+        expected = V.jsetsToCsv journals . J.yearly26Sets 2019 $ refs
     etJSets <- P.parseCollection refs <$> Tx.readFile "tests/res/JSet2019.csv"
     case etJSets of
          Left err    -> error err
-         Right jSets -> F.jsetsToCsv journals jSets `shouldBe` expected
+         Right jSets -> V.jsetsToCsv journals jSets `shouldBe` expected
