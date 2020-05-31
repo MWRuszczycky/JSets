b4891b086685d983225ce5ed00d26ac067b4e3ed
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-31 00:14:47 -0500

    Fixes tests

---
+++ tests/Mock.hs
@@ -0,0 +1,16 @@
+module Mock
+    ( runView
+    ) where
+
+import qualified Model.Core.Types     as T
+import qualified Data.Text            as Tx
+import           Data.Text                  ( Text           )
+import           Data.Monoid                ( appEndo        )
+import           Control.Monad.Reader       ( runReader      )
+import           Control.Monad.Writer       ( execWriterT    )
+
+runView :: T.ViewMonad a -> Text
+runView = Tx.concat
+          . flip appEndo []
+          . flip runReader T.defaultConfig
+          . execWriterT
--- tests/ParserTests.hs
+++ tests/ParserTests.hs
@@ -8,6 +8,7 @@ import qualified Model.Core.Types          as T
 import qualified Model.Journals            as J
 import qualified Model.Parsers.JournalSets as P
 import qualified View.View                 as V
+import qualified Mock                      as Mock
 import qualified TestReferences            as TR
 import           Test.Hspec                       ( Spec (..)
                                                   , hspec
@@ -21,11 +22,17 @@ spec = hspec $ do
         testGenParse2019
 
 testGenParse2019 :: Spec
-testGenParse2019 = it "Parses JSet2019 to yearly26Sets 2019" $ do
-    let refs     = TR.issueRefs
-        journals = map (T.abbr . T.journal) refs
-        expected = V.jsetsToCsv journals . J.yearly26Sets 2019 $ refs
-    etJSets <- P.parseJSets refs <$> Tx.readFile "tests/res/JSet2019.csv"
-    case etJSets of
-         Left err    -> error err
-         Right jSets -> V.jsetsToCsv journals jSets `shouldBe` expected
+testGenParse2019 = do
+    let refs       = TR.issueRefs
+        yearly2019 = J.yearly26Sets 2019 refs
+        abbrs      = map (T.abbr . T.journal) refs
+        viewCsv    = Mock.runView . V.jsetsToCsv abbrs $ yearly2019
+    it "views yearly26Sets correctly as csv" $ do
+        expectedCsv <- Tx.readFile "tests/res/JSet2019.csv"
+        viewCsv `shouldBe` expectedCsv
+    it "parses yearly26Sets correctly as csv" $ do
+        etJSets <- P.parseJSets refs <$> Tx.readFile "tests/res/JSet2019.csv"
+        case etJSets of
+             Left err    -> error err
+             Right jsets -> Mock.runView (V.jsetsToCsv abbrs jsets)
+                            `shouldBe` viewCsv
