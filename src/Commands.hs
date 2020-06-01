4f4123953a97b8b5469c12c9d7688df1ddd228e3
MWRuszczycky <mwruszczycky@gmail.com>
2020-05-31 19:57:47 -0500

    Uses FileEmbed to manage help strings for commands

--- src/Commands.hs
+++ src/Commands.hs
@@ -48,13 +48,8 @@ runCommands (x:xs) = maybe err go . find ( (==x) . T.cmdName ) $ commands
 -- Providing help information
 
 helpHelp :: (Text, Text)
-helpHelp = (s, Tx.unlines hs)
+helpHelp = (s, H.helpHelp)
     where s  = "display help information for a command (e.g., help refs)"
-          hs = [ "Usage: help <command-name>"
-               , "Displays detailed information for a command."
-               , "To see a help information summary and complete list of"
-               , "available commands, use the --help/-h flag."
-               ]
 
 helpCmd :: [String] -> T.AppMonad ()
 helpCmd []    = helpCmd ["help"]
@@ -66,13 +61,8 @@ helpCmd (c:_) = maybe err go . find ( (==c) . T.cmdName ) $ commands
 -- Downloading raw json from pubmed
 
 jsonHelp :: (Text, Text)
-jsonHelp = (s, Tx.unlines hs)
-    where s  = "download raw json for a journal issue (for debugging)"
-          hs = [ "Usage: json <Journal-abbreviation> <volume> <issue>"
-               , "Generates two files:"
-               , "    esearch.json:  the esearch response"
-               , "    esummary.json: the esummary response"
-               ]
+jsonHelp = (s, H.jsonHelp)
+    where s  = "download raw PubMed json responses for a journal issue"
 
 jsonCmd :: [String] -> T.AppMonad ()
 jsonCmd [] = throwError "A journal issue must be specified."
@@ -93,12 +83,8 @@ jsonCmd xs
 -- Generating output for ranking articles
 
 ranksHelp :: (Text, Text)
-ranksHelp = (s, Tx.unlines hs)
+ranksHelp = (s, H.ranksHelp)
     where s  = "output a list of selected articles generating a ranklist"
-          hs = [ "Usage:"
-               , "  ranks FILE [FILE]"
-               , "where FILE is a selections file."
-               ]
 
 ranksCmd :: [FilePath] -> T.AppMonad ()
 ranksCmd [] = throwError "Path(s) to journal set selection files required!"
@@ -141,26 +127,8 @@ checkForMissingCitations ics
 -- File reading and conversion
 
 readHelp :: (Text, Text)
-readHelp = (s, Tx.unlines hs)
+readHelp = (s, H.readHelp)
     where s  = "read journal sets from file"
-          hs = [ "A journal set is a collection of journal issues for review."
-               , "Collections of journal sets can be read from a file in either"
-               , "text or csv format using the <read> command. A specific set"
-               , "can be read alone by specifying the key (a positive integer)"
-               , "of the specific journal set of interest using the --key/-k"
-               , "option. For example, if <jsets2019.txt> is a file containing"
-               , "all the journal sets for the year 2019, then you can print"
-               , "journal set 5 to the terminal using,\n"
-               , "    jsets read jsets2019.txt --key=5\n"
-               , "The default output is formatted as text. You can use the"
-               , "<read> command to convert between journal set formats by"
-               , "using an explicit output path and indicating the format with"
-               , "the output file extension. So, if you want to convert a text"
-               , "file to csv, then you would run,\n"
-               , "    jsets read jsets2019.txt --output=jsets2019.csv\n"
-               , "If you want to convert the csv file to text, then run,\n"
-               , "    jsets read jsets2019.csv --output=jsets2019.txt"
-               ]
 
 readCmd :: [FilePath] -> T.AppMonad ()
 readCmd []  = throwError "Path(s) to journal sets files are required!"
@@ -173,10 +141,8 @@ readCmd fps = do
 -- View configured journals
 
 refsHelp :: (Text, Text)
-refsHelp = (s, Tx.unlines hs)
+refsHelp = (s, H.refsHelp)
     where s  = "list configured journals and reference issues"
-          hs = [ "Usage: jsets refs"
-               ]
 
 refsCmd :: [String] -> T.AppMonad ()
 refsCmd _ = do
@@ -189,28 +155,8 @@ refsCmd _ = do
 -- Download tables of contents for all issues in a journal set
 
 tocHelp :: (Text, Text)
-tocHelp = (s, Tx.unlines hs)
+tocHelp = (s, H.tocHelp)
     where s  = "generate tables of contents for a journal set"
-          hs = [ "If jsets2019.txt is a collection of journal sets (see <read>"
-               , "command), then you can generate a file of the tables of"
-               , "contents for each issue in the set with key 6 using,\n"
-               , "    jsets toc jsets2019.txt --key=6\n"
-               , "The default output is text and will be printed to the"
-               , "terminal. To generate the html output for easy selection, set"
-               , "the extension of the output file to 'html', for example,\n"
-               , "    jsets toc jsets2019.txt --key=6 --output=toc2019-6.html\n"
-               , "The tables of contents are downloaded from PubMed. To obtain"
-               , "the raw, unparsed PubMed data, use the extension 'raw',"
-               , "for example,\n"
-               , "    jsets toc jsets2019.txt --key=6 --output=toc2019-6.raw\n"
-               , "Markdown formatting is available with the 'mkd' extension,\n"
-               , "    jsets toc jsets2019.txt --key=6 --output=toc2019-6.mkd\n"
-               , "When reading from a selection file you need to set a style"
-               , "flag to indicate that a selection file is being parsed and"
-               , "the type of table of contents information to generate:\n"
-               , "    --select/-s : toc for selecting citations for review"
-               , "    --rank/-r   : toc for ranking citations"
-               ]
 
 tocCmd :: [FilePath] -> T.AppMonad ()
 tocCmd []  = throwError "Path(s) to journal sets files are required!"
@@ -224,16 +170,8 @@ tocCmd fps = do
 -- Construct journal set collections by year
 
 yearHelp :: (Text, Text)
-yearHelp = (s, Tx.unlines hs)
+yearHelp = (s, H.yearHelp)
     where s  = "build a collection of all journal sets in a given year"
-          hs = [ "The <year> command distributes all issues for all configured"
-               , "journals in a given year into 26 journal sets. So, to create"
-               , "a file with the default journal sets in 2019 use,\n"
-               , "    jsets year 2019 --output=jsets2019.txt\n"
-               , "The default output format is text. To format the file as csv,"
-               , "set an output path with a 'csv' extension. For example,\n"
-               , "    jsets year 2019 --output=jsets2019.csv"
-               ]
 
 yearCmd :: [String] -> T.AppMonad ()
 yearCmd []    = throwError "A valid year must be specified!"
--- src/View/Help.hs
+++ src/View/Help.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module View.Help
     ( summary
     , details
     , version
+      -- Help strings
+    , helpHelp
+    , jsonHelp
+    , ranksHelp
+    , readHelp
+    , refsHelp
+    , tocHelp
+    , yearHelp
     ) where
 
 import qualified Data.Text             as Tx
 import qualified System.Console.GetOpt as Opt
+import qualified Data.FileEmbed        as FE
 import qualified Model.Core.Types      as T
 import qualified Data.Version          as Ver
 import qualified Paths_lab_schedule    as Paths
@@ -39,7 +49,31 @@ cmdSummary ( T.Command name _ (hs,_) ) = paddedName <> " : " <> hs
     where paddedName = Tx.pack name <> Tx.replicate (5 - length name) " "
 
 cmdHeader :: T.Command -> Text
-cmdHeader cmd = Tx.replicate 60 "-" <> "\n-- " <> cmdSummary cmd <> "\n"
+cmdHeader cmd = Tx.replicate 20 "-" <> "\n  " <> cmdSummary cmd <> "\n"
 
 optionsHelp :: [Opt.OptDescr (T.Config -> T.ErrMonad T.Config)] -> Text
 optionsHelp = Tx.pack . Opt.usageInfo "Options summary:"
+
+-- =============================================================== --
+-- Help strings
+
+helpHelp :: Text
+helpHelp = $(FE.embedStringFile "res/help/helpHelp.txt")
+
+jsonHelp :: Text
+jsonHelp = $(FE.embedStringFile "res/help/jsonHelp.txt")
+
+ranksHelp :: Text
+ranksHelp = $(FE.embedStringFile "res/help/ranksHelp.txt")
+
+readHelp :: Text
+readHelp = $(FE.embedStringFile "res/help/readHelp.txt")
+
+refsHelp :: Text
+refsHelp = $(FE.embedStringFile "res/help/refsHelp.txt")
+
+tocHelp :: Text
+tocHelp = $(FE.embedStringFile "res/help/tocHelp.txt")
+
+yearHelp :: Text
+yearHelp = $(FE.embedStringFile "res/help/yearHelp.txt")
