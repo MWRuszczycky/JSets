03cbf58be8b657263e309778347b4cceaf0a368c
MWRuszczycky <mwruszczycky@gmail.com>
2020-07-11 13:45:16 -0500

    Refactors tests for Model.Journals

--- tests/ModelSpec/Journals.hs
+++ tests/ModelSpec/Journals.hs
@@ -19,21 +19,23 @@ import           Test.Hspec                  ( Spec (..)
 
 spec :: IO ()
 spec = hspec $ do
-    describe "Testing current issue (issueAtDate)" $ do
-        earlyDates
-        testScience
-        testJacs
-        testCellChemBiol
-    describe "addCitations & missingPMIDs" $ do
-        it "addCitations works with no missing PMIDs" $
-            addCitationsNoMissingSpec
-        it "addCitations & missingPMIDs work with missing PMIDs" $
-            addCitationsMissingSpec
-    describe "rank match scoring (score)" $ do
-        scoreSpec
+    describe "Model.Journals.issueAtDate" $ do
+        spec_issueAtDate
+    describe "Model.Journals.addCitations" $ do
+        spec_addCitations
+    describe "Model.Journals.missingPMIDs" $ do
+        spec_missingPMIDs
+    describe "Model.Journals.score" $ do
+        spec_score
+
+-- =============================================================== --
+-- Working with journal sets
 
 ---------------------------------------------------------------------
--- Testing Model.Journals.issueAtDate
+-- Model.Journals.yearly26Sets
+
+-- =============================================================== --
+-- Working with journal issues
 
 checkVolNum :: T.Issue -> Int -> Int -> IO ()
 checkVolNum x v n = (T.volNo x, T.issNo x) `shouldBe` (v, n)
@@ -43,9 +45,18 @@ scienceRef      = let (Just x) = TR.refIssue "Science" in x
 jacsRef         = let (Just x) = TR.refIssue "JACS" in x
 cellChemBiolRef = let (Just x) = TR.refIssue "Cell Chem Biol" in x
 
-earlyDates :: Spec
-earlyDates = do
-    it "Handles early dates for Science correctly" $ do
+---------------------------------------------------------------------
+-- Model.Journals.issueAtDate
+
+spec_issueAtDate :: Spec
+spec_issueAtDate = do
+    test_IssuesForScience
+    test_IssuesForJACS
+    test_IssuesForCellChemBiol
+
+test_IssuesForScience :: Spec
+test_IssuesForScience = do
+    it "identifies issues near Science reference correctly" $ do
         let n1 = J.issueAtDate (Tm.fromGregorian 2009 12 31) scienceRef
             n2 = J.issueAtDate (Tm.fromGregorian 2010  1  1) scienceRef
             n3 = J.issueAtDate (Tm.fromGregorian 2010  1  2) scienceRef
@@ -54,28 +65,7 @@ earlyDates = do
         checkVolNum n1 2010 5961
         checkVolNum n1 2010 5961
         checkVolNum n4 2010 5978
-    it "Handles early dates for JACS correctly" $ do
-        let n1 = J.issueAtDate (Tm.fromGregorian 2009 12 31) jacsRef
-            n2 = J.issueAtDate (Tm.fromGregorian 2010  1 13) jacsRef
-            n3 = J.issueAtDate (Tm.fromGregorian 2010  1 14) jacsRef
-            n4 = J.issueAtDate (Tm.fromGregorian 2010  5  6) jacsRef
-        checkVolNum n1 132 1
-        checkVolNum n1 132 1
-        checkVolNum n1 132 1
-        checkVolNum n4 132 17
-    it "Handles early dates for Cell Chem Biol correctly" $ do
-        let n1 = J.issueAtDate (Tm.fromGregorian 2009 12 31) cellChemBiolRef
-            n2 = J.issueAtDate (Tm.fromGregorian 2010  1  1) cellChemBiolRef
-            n3 = J.issueAtDate (Tm.fromGregorian 2010  1  2) cellChemBiolRef
-            n4 = J.issueAtDate (Tm.fromGregorian 2010  5  6) cellChemBiolRef
-        checkVolNum n1 17 1
-        checkVolNum n1 17 1
-        checkVolNum n1 17 1
-        checkVolNum n4 17 4
-
-testScience :: Spec
-testScience = do
-    it "Identifies Science 2015-2016-2017 issues correctly" $ do
+    it "identifies Science 2015-2016-2017 issues correctly" $ do
         let n1  = J.issueAtDate (Tm.fromGregorian 2015 12 16) scienceRef
             n2  = J.issueAtDate (Tm.fromGregorian 2015 12 18) scienceRef
             n3  = J.issueAtDate (Tm.fromGregorian 2015 12 19) scienceRef
@@ -102,7 +92,7 @@ testScience = do
         checkVolNum n11 2016 6319
         checkVolNum n12 2016 6319
         checkVolNum n13 2017 6320
-    it "Identifies Science 2018-2019-2020 issues correctly" $ do
+    it "identifies Science 2018-2019-2020 issues correctly" $ do
         let n1  = J.issueAtDate (Tm.fromGregorian 2018 12 18) scienceRef
             n2  = J.issueAtDate (Tm.fromGregorian 2018 12 21) scienceRef
             n3  = J.issueAtDate (Tm.fromGregorian 2019 1   4) scienceRef
@@ -130,14 +120,23 @@ testScience = do
         checkVolNum n12 2019 6472
         checkVolNum n13 2020 6473
 
-testJacs :: Spec
+test_IssuesForJACS :: Spec
 -- ^Actual JACS publication dates are not always consistent and can
 -- come either 6 or 8 days, but average out to 7. For example, the
 -- last issue of 2013 was on December 26, but it should have been on
 -- December 25. Tests assume JACS is always 7 days between issues.
 -- So, the December 25 day is used in the tests.
-testJacs = do
-    it "Identifies JACS 2013-2014-2015 issues correctly" $ do
+test_IssuesForJACS = do
+    it "identifies issues near JACS reference correctly" $ do
+        let n1 = J.issueAtDate (Tm.fromGregorian 2009 12 31) jacsRef
+            n2 = J.issueAtDate (Tm.fromGregorian 2010  1 13) jacsRef
+            n3 = J.issueAtDate (Tm.fromGregorian 2010  1 14) jacsRef
+            n4 = J.issueAtDate (Tm.fromGregorian 2010  5  6) jacsRef
+        checkVolNum n1 132 1
+        checkVolNum n1 132 1
+        checkVolNum n1 132 1
+        checkVolNum n4 132 17
+    it "identifies JACS 2013-2014-2015 issues correctly" $ do
         let n1  = J.issueAtDate (Tm.fromGregorian 2013 12 24) jacsRef
             n2  = J.issueAtDate (Tm.fromGregorian 2013 12 25) jacsRef
             n3  = J.issueAtDate (Tm.fromGregorian 2013 12 31) jacsRef
@@ -164,7 +163,7 @@ testJacs = do
         checkVolNum n11 136 52
         checkVolNum n12 136 52
         checkVolNum n13 137  1
-    it "Identifies JACS 2015-2016-2017 issues correctly" $ do
+    it "identifies JACS 2015-2016-2017 issues correctly" $ do
         let n1  = J.issueAtDate (Tm.fromGregorian 2015 12 29) jacsRef
             n2  = J.issueAtDate (Tm.fromGregorian 2015 12 30) jacsRef
             n3  = J.issueAtDate (Tm.fromGregorian 2015 12 31) jacsRef
@@ -192,9 +191,18 @@ testJacs = do
         checkVolNum n12 138 51
         checkVolNum n13 139  1
 
-testCellChemBiol :: Spec
-testCellChemBiol = do
-    it "Identifies Cell Chem Biol 2015-2016-2017 issues correctly" $ do
+test_IssuesForCellChemBiol :: Spec
+test_IssuesForCellChemBiol = do
+    it "identifies issues near Cell Chem Biol reference correctly" $ do
+        let n1 = J.issueAtDate (Tm.fromGregorian 2009 12 31) cellChemBiolRef
+            n2 = J.issueAtDate (Tm.fromGregorian 2010  1  1) cellChemBiolRef
+            n3 = J.issueAtDate (Tm.fromGregorian 2010  1  2) cellChemBiolRef
+            n4 = J.issueAtDate (Tm.fromGregorian 2010  5  6) cellChemBiolRef
+        checkVolNum n1 17 1
+        checkVolNum n1 17 1
+        checkVolNum n1 17 1
+        checkVolNum n4 17 4
+    it "identifies Cell Chem Biol 2015-2016-2017 issues correctly" $ do
         let n1 = J.issueAtDate (Tm.fromGregorian 2015 12 16) cellChemBiolRef
             n2 = J.issueAtDate (Tm.fromGregorian 2016 1   1) cellChemBiolRef
             n3 = J.issueAtDate (Tm.fromGregorian 2016 2   1) cellChemBiolRef
@@ -214,8 +222,8 @@ testCellChemBiol = do
         checkVolNum n8 23 12
         checkVolNum n9 24 1
 
---------------------------------------------------------------------
--- Testing the construction of IssueContents
+-- =============================================================== --
+-- Working with selection sets
 
 testBV58N49 :: IO (T.Issue, [T.Citation])
 -- ^Citations for Biochemistry (2019) Volume 58 Issue 49.
@@ -239,6 +247,14 @@ checkAddCitations (T.Content sel cs, rest) allpmids = do
     added                    `shouldBe` selected
     sort (added <> notAdded) `shouldBe` allpmids
 
+---------------------------------------------------------------------
+-- Model.Journals.addCitations
+
+spec_addCitations :: Spec
+spec_addCitations = do
+    it "works with no missing PMIDs"
+        addCitationsNoMissingSpec
+
 addCitationsNoMissingSpec :: IO ()
 addCitationsNoMissingSpec = do
     (iss, cites) <- testBV58N49
@@ -251,6 +267,14 @@ addCitationsNoMissingSpec = do
     checkAddCitations (J.addCitations (toSel sel1)  cites) pmids
     checkAddCitations (J.addCitations (toSel sel3)  cites) pmids
     checkAddCitations (J.addCitations (toSel pmids) cites) pmids
+
+---------------------------------------------------------------------
+-- Model.Journals.missingPMIDs
+
+spec_missingPMIDs :: Spec
+spec_missingPMIDs = do
+    it "(with addCitations) works with and without missing PMIDs"
+        addCitationsMissingSpec
 
 addCitationsMissingSpec :: IO ()
 addCitationsMissingSpec = do
@@ -273,30 +297,33 @@ addCitationsMissingSpec = do
     (J.missingPMIDs . fst $ J.addCitations (toSel seln) cites)
         `shouldBe` [ "00000000", "11111111" ]
 
----------------------------------------------------------------------
--- Rank matching
+-- =============================================================== --
+-- Working with rank matching
 
-scoreSpec :: Spec
-scoreSpec = do
+---------------------------------------------------------------------
+-- Model.Journelas.score
+
+spec_score :: Spec
+spec_score = do
     let r1 = [ [1], [5,9,2], [3,11,4], [6], [7,13], [8] ]
         r2 = [ [1], [5], [9], [2], [3], [11], [4], [6], [7], [13], [8] ]
         e1 = [ (1,4), (2,3), (3,1), (4,1) ]
         e2 = [ (1,1), (2,1), (3,1), (4,1) ]
         e3 = [ (5,7), (9,7), (2,7), (3,6), (11,6), (6,2), (8,1) ]
         e4 = [ (5,9), (9,9), (2,9), (3,8), (11,8), (10,3), (12,3), (6,2), (8,1) ]
-    it "Works with empty rankings" $ do
+    it "works with no ranked papers" $ do
         J.score [1,2,3,4] [] `shouldBe` e2
-    it "Works with empty indices" $
+    it "works with no indexed papers" $
         J.score [] r1 `shouldBe` []
-    it "Works with single ranking, but not an index" $ do
+    it "works with a single paper ranked that is not an indexed paper" $ do
         J.score [1,2,3,4] [[6]] `shouldBe` e2
-    it "Works with single ranking, and is an index" $ do
+    it "works with a single paper ranked that is an indexed paper" $ do
         J.score [1,2,3,4] [[3]] `shouldBe` [ (3,4), (1,1), (2,1), (4,1) ]
-    it "Works with single ranking, and is an index group" $ do
+    it "works with a 4 indexed papers and only 3 are ranked (equally)" $ do
         J.score [1,2,3,4] [[3,4,1]] `shouldBe` [ (3,4), (4,4), (1,4), (2,1) ]
-    it "Works with multiple selections ranked singly" $ do
+    it "works with 4 indexed papers and 11 singly ranked papers" $ do
         J.score [2,4,5,9] r2 `shouldBe` [ (5,4), (9,3), (2,2), (4,1) ]
-    it "Works with several different sets of test indices" $ do
+    it "works with mixed rank-lists and different sets of indexed papers" $ do
         J.score [1,2,3,4]              r1 `shouldBe` e1
         J.score [2,5,9,3,11,6,8]       r1 `shouldBe` e3
         J.score [2,5,9,3,11,6,8,10,12] r1 `shouldBe` e4
