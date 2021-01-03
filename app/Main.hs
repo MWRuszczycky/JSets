40539136858294e0e7b3fd5f3835ae9a6fbcfb26
MWRuszczycky <mwruszczycky@gmail.com>
2021-01-03 13:30:17 -0600

    Adds proper exit code when execution fails

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,15 @@
 module Main where
 
 import qualified GHC.IO.Encoding      as GHC
-import           Control.Monad.Except        ( runExceptT   )
-import           Controller                  ( configureApp
-                                             , runApp       )
+import           System.Exit                  ( die          )
+import           Control.Monad.Except         ( runExceptT   )
+import           Controller                   ( configureApp
+                                              , runApp       )
 
 main :: IO ()
 main = do
     GHC.setLocaleEncoding GHC.utf8
     result <- runExceptT $ configureApp >>= runApp
     case result of
-         Left err -> putStrLn $ err <> "\nTry '-h' or '--help' for usage."
          Right _  -> pure ()
+         Left err -> die $ err <> "\nTry '-h' or '--help' for usage."
